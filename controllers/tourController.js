db2d0e92ff80a1641d83d4c2bd7c9a59fee7da96
Gehan Sithija <157275598+Gehan1226@users.noreply.github.com>
2025-04-11 22:18:09 +0530

    Refactor tour controller and routes; implement Mongoose model for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,39 +1,6 @@
-const fs = require("fs");
+const Tour = require("./../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tour-data.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-}
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Missing name or price",
-    });
-  }
-  next();
-}
-
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
+exports.getAllTours = (req, res) => {};
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
@@ -54,47 +21,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = parseInt(tours[tours.length - 1].id) + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tour-data.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({
-          status: "fail",
-          message: "Error writing to file",
-        });
-        return;
-      }
-      res.status(201).json({
-        status: "success",
-        data: {
-          tours,
-        },
-      });
-    }
-  );
-};
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: "fail",
-      message: "Tour not found",
+      message: "Invalid data sent",
     });
   }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "Tour updated successfully",
-    },
-  });
 };
+
+exports.updateTour = (req, res) => {};
